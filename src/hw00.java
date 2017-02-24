99903bcbc846c0982f7097f92c08d6b43ef80b07
LRouo <105021002@live.asia.edu.tw>
2017-02-24 14:57:19 +0800

    submit2

---
+++ src/hw00.java
@@ -0,0 +1,16 @@
+/*Topic:第一次作業繳交 自我介紹 
+ * Date:2017/02/24
+ * Author: 105021002 賴映如
+ */
+public class hw00 {
+
+	public static void main(String[] args){
+		
+		System.out.println("你的名字:賴映如"); //
+		System.out.println("學號:105021002");
+		System.out.println("系級:資工資電1A");
+		
+	}
+
+}
+
